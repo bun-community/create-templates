41bb689d10e979627ec6db30dfbbce02785916c6
Dennis Dudek <admin@kapsonfire.de>
2022-10-13 22:46:04 +0200

    add svelte-kit template

---
+++ templates/svelte-kit/svelte.config.js
@@ -0,0 +1,19 @@
+import adapter from '@bun-community/sveltekit-adapter-bun';
+import preprocess from 'svelte-preprocess';
+
+console.log(process.version);
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter({
+							 dynamic_origin: true
+						 })
+	}
+};
+
+export default config;
