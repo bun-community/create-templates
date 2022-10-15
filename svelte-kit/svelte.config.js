19aa5c974970651ac04a12caf51d7c2b400c99cb
Dennis Dudek <admin@kapsonfire.de>
2022-10-15 09:58:10 +0200

    remove useless log

--- svelte-kit/svelte.config.js
+++ svelte-kit/svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@bun-community/sveltekit-adapter-bun';
 import preprocess from 'svelte-preprocess';
 
-console.log(process.version);
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
