613581b53b1dfb505b58e7f16465f0455fd6faf4
Revenity <aquaplmc@gmail.com>
2023-02-26 11:44:56 +0700

    Update index.ts

--- stric/index.ts
+++ stric/index.ts
@@ -1,5 +1,7 @@
 import Stric from "@stricjs/kit";
 
 // This is a shorthand call, use all the options in ./src/stric.config.json
-// Options specified in the code will be merged with all options in the config file
 await Stric.boot();
+
+// Log app mode
+console.log("App is running in", Bun.env.NODE_ENV, "mode.");
