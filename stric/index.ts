734e7cee65882e7a822459e1e565eb5e04becd62
Revenity <aquaplmc@gmail.com>
2023-02-26 07:33:56 +0700

    Update index.ts

--- stric/index.ts
+++ stric/index.ts
@@ -1,6 +1,5 @@
 import Stric from "@stricjs/kit";
 
-// This is a shorthand call, use all the options in (root)/stric.config.json
+// This is a shorthand call, use all the options in ./src/stric.config.json
 // Options specified in the code will be merged with all options in the config file
-await Stric.boot({ root: import.meta.dir + "/src" });
-await Stric.boot({ root: import.meta.dir + "/src" });
+await Stric.boot();
