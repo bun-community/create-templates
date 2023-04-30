daeac18b738144495e6d671907dd67b4d98d2d95
Revenity <aquaplmc@gmail.com>
2023-04-30 14:15:19 +0700

    Update App.ts

--- stric-kit/src/pages/App.ts
+++ stric-kit/src/pages/App.ts
@@ -1,5 +1,12 @@
 import { html } from "@stricjs/arrow/utils";
 
+// Render on the client
 export function render() {
-    html`<p>Hello world</p>`;
+    // Render a <p> tag with text "Hi"
+    html`<p>Hi</p>`;
+    
+    // Code inside this can use Web APIs
 }
+
+// Export a path to handle
+export const path = "/";
