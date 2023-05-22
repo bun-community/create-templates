958876c3f38aa5a2b5c1555eccef0b3c81a30f06
Colin McDonnell <colinmcd94@gmail.com>
2023-05-22 12:26:03 -0700

    Add web types

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,13 +1,13 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <App />,
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
