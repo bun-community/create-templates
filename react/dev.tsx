61ae7d103cd59f41829cece3c22993d01881694c
Arun Prabhu K <arunprabhuk94@gmail.com>
2023-08-23 09:48:08 +0530

    feat(react): temporary fix for hot reloading

--- react/dev.tsx
+++ react/dev.tsx
@@ -1,15 +1,11 @@
 import * as path from "path";
 import { statSync } from "fs";
-import type { ServeOptions } from "bun";
+import { withHtmlLiveReload } from "bun-html-live-reload";
 
 const PROJECT_ROOT = import.meta.dir;
 const PUBLIC_DIR = path.resolve(PROJECT_ROOT, "public");
 const BUILD_DIR = path.resolve(PROJECT_ROOT, "build");
-
-await Bun.build({
-  entrypoints: ["./src/index.tsx"],
-  outdir: "./build",
-});
+const SRC_DIR = path.resolve(PROJECT_ROOT, "src");
 
 function serveFromDir(config: {
   directory: string;
@@ -31,27 +27,37 @@ function serveFromDir(config: {
   return null;
 }
 
-const server = Bun.serve({
-  fetch(request) {
-    let reqPath = new URL(request.url).pathname;
-    console.log(request.method, reqPath);
-    if (reqPath === "/") reqPath = "/index.html";
+export default withHtmlLiveReload(
+  {
+    fetch(request) {
+      let reqPath = new URL(request.url).pathname;
+      console.log(request.method, reqPath);
+      if (reqPath === "/") reqPath = "/index.html";
 
-    // check public
-    const publicResponse = serveFromDir({
-      directory: PUBLIC_DIR,
-      path: reqPath,
-    });
-    if (publicResponse) return publicResponse;
+      // check public
+      const publicResponse = serveFromDir({
+        directory: PUBLIC_DIR,
+        path: reqPath,
+      });
+      if (publicResponse) return publicResponse;
 
-    // check /.build
-    const buildResponse = serveFromDir({ directory: BUILD_DIR, path: reqPath });
-    if (buildResponse) return buildResponse;
+      // check /.build
+      const buildResponse = serveFromDir({
+        directory: BUILD_DIR,
+        path: reqPath,
+      });
+      if (buildResponse) return buildResponse;
 
-    return new Response("File not found", {
-      status: 404,
-    });
+      return new Response("File not found", {
+        status: 404,
+      });
+    },
   },
-});
-
-console.log(`Listening on http://localhost:${server.port}`);
+  {
+    buildConfig: {
+      entrypoints: ["./src/index.tsx"],
+      outdir: "./build",
+    },
+    watchPath: SRC_DIR,
+  }
+);
